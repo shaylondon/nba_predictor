f054f9b74bdbbf5596c9808c5636efe6eee15a5b
shay-london@uiowa.edu <112662764+shaylondon@users.noreply.github.com>
2024-10-01 17:22:38 -0500

    Separated pace and efg last 10

--- predictor.py
+++ predictor.py
@@ -1,12 +1,19 @@
 from nba_api.stats.endpoints import leaguedashteamstats
+import pandas as pd
 
 def main():
-    team_advanced_stats = leaguedashteamstats.LeagueDashTeamStats(last_n_games=10,measure_type_detailed_defense='Advanced')
+    team_advanced_stats_last_10 = leaguedashteamstats.LeagueDashTeamStats(last_n_games=10,measure_type_detailed_defense='Advanced',season="2022-23")
     
-    team_advanced_stats_df = team_advanced_stats.get_data_frames()[0]
+    team_advanced_stats_last_10_df = team_advanced_stats_last_10.get_data_frames()[0]
     
-    pace_efg = team_advanced_stats_df[["TEAM_NAME", "EFG_PCT","PACE"]].sort_values(by='TEAM_NAME')
+    pace_last_10 = team_advanced_stats_last_10_df[["TEAM_NAME","PACE_RANK"]].sort_values(by='PACE_RANK')[0:10]
+
+    efg_last_10 = team_advanced_stats_last_10_df[["TEAM_NAME","EFG_PCT_RANK"]].sort_values(by='EFG_PCT_RANK')[0:10]
+
+    print(pace_last_10.set_index("TEAM_NAME").join(efg_last_10.set_index("TEAM_NAME")))
     
-    print(pace_efg)
+    for team in efg_last_10["TEAM_NAME"]:
+        if any(pace_last_10["TEAM_NAME"] == team):
+            print(team)
     
 if __name__ == "__main__": main()
