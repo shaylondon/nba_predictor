2156d6183763ca951b1111a2f0887dcba57329d4
shay-london@uiowa.edu <112662764+shaylondon@users.noreply.github.com>
2024-11-19 22:30:12 -0600

    added four factors stat retrieval and removed superfluous functions

--- predictors/4factors_moneyline.py
+++ predictors/4factors_moneyline.py
@@ -21,56 +21,46 @@ def get_total_lines() -> list:
         lines.append([team1, team2, dk_total, fd_total])
     return lines
 
-def overs_last_n_games(top_n_teams: int, last_n_games=None) -> list:
-    top_10_efg_last_n_games = efg_last_n_games(last_n_games)[:top_n_teams]
-    top_10_pace_last_n_games = pace_last_n_games(last_n_games)[:top_n_teams]
-    matchups = get_matchups()
-    teams_playing = get_teams_playing()
+def get_team_4factors_stats_last_n_games(games: int) -> DataFrame:
+    if games is None:
+        team_4factors_stats = leaguedashteamstats.LeagueDashTeamStats()
+    else:
+        team_4factors_stats = leaguedashteamstats.LeagueDashTeamStats(last_n_games=games,
+                                                                      measure_type_detailed_defense='Four Factors')
 
-    last_10_efg_matchups = [entry.split(' ')[-1] for entry in top_10_efg_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
-    last_10_pace_matchups = [entry.split(' ')[-1] for entry in top_10_pace_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
+    return team_4factors_stats.get_data_frames()[0]
 
-    return [matchup for matchup in matchups if (matchup[0] in last_10_efg_matchups and matchup[1] in last_10_pace_matchups) or (matchup[0] in last_10_pace_matchups and matchup[1] in last_10_efg_matchups)]
+# def overs_last_n_games(top_n_teams: int, last_n_games=None) -> list:
+#     top_10_efg_last_n_games = efg_last_n_games(last_n_games)[:top_n_teams]
+#     top_10_pace_last_n_games = pace_last_n_games(last_n_games)[:top_n_teams]
+#     matchups = get_matchups()
+#     teams_playing = get_teams_playing()
+#
+#     last_10_efg_matchups = [entry.split(' ')[-1] for entry in top_10_efg_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
+#     last_10_pace_matchups = [entry.split(' ')[-1] for entry in top_10_pace_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
+#
+#     return [matchup for matchup in matchups if (matchup[0] in last_10_efg_matchups and matchup[1] in last_10_pace_matchups) or (matchup[0] in last_10_pace_matchups and matchup[1] in last_10_efg_matchups)]
 
-def efg_last_n_games(games: int) -> DataFrame:
-    team_advanced_stats = get_team_advanced_stats_last_n_games(games)
-    efg = team_advanced_stats[['TEAM_NAME','EFG_PCT_RANK', 'EFG_PCT']].sort_values(by='EFG_PCT_RANK')
-    print(efg[:10])
-
-    return efg
-
-def pace_last_n_games(games: int) -> DataFrame:
-    team_advanced_stats = get_team_advanced_stats_last_n_games(games)
-    pace = team_advanced_stats[['TEAM_NAME','PACE_RANK', 'PACE']].sort_values(by='PACE_RANK')
-    print(pace[:10])
-
-    return pace
-
-def team_ppg_last_n_games(team_names: list, games=None) -> list:
-    teams_ppg: list = []
-    team_stats_df = get_team_stats_last_n_games(games)
-    team_stats_df['TEAM_PPG']= team_stats_df.apply(lambda row: round(row.PTS / row.GP, 1), axis=1)
-    team_stats_df['TEAM_NAME'] = team_stats_df.apply(lambda row: row.TEAM_NAME.split(' ')[-1], axis=1)
-    team_ppg_df = team_stats_df[['TEAM_NAME','TEAM_PPG']]
-    teams_ppg.append([team_ppg_df.loc[team_ppg_df['TEAM_NAME'] == team_names[0]].iat[0,0], team_ppg_df.loc[team_ppg_df['TEAM_NAME'] == team_names[0]].iat[0,1]])
-    teams_ppg.append([team_ppg_df.loc[team_ppg_df['TEAM_NAME'] == team_names[1]].iat[0,0], team_ppg_df.loc[team_ppg_df['TEAM_NAME'] == team_names[1]].iat[0,1]])
-    return teams_ppg
+# def efg_last_n_games(games: int) -> DataFrame:
+#     team_advanced_stats = get_team_advanced_stats_last_n_games(games)
+#     efg = team_advanced_stats[['TEAM_NAME','EFG_PCT_RANK', 'EFG_PCT']].sort_values(by='EFG_PCT_RANK')
+#     print(efg[:10])
+#
+#     return efg
 
 def main():
     last_n_games: int = int(input('Last _ Games: '))
-    top_n_teams: int = int(input('Top _ Teams: '))
-    matchups = overs_last_n_games(top_n_teams,last_n_games)
+    matchups = overs_last_n_games(last_n_games)
     if not matchups:
         print('No matchups fit criteria on this date.')
         return
-    matchups_with_pts = [avg_total_pts_last_n_games(matchup, last_n_games) for matchup in matchups]
-    for over_game in matchups_with_pts:
+    for over_game in matchups:
         for game in get_total_lines():
             if over_game[0] == game[0]:
                 over_game.append(game[2])
                 over_game.append(game[3])
 
-    df = pd.DataFrame(matchups_with_pts,columns=['Away Team','Home Team',f'Last {last_n_games} Games Avg Total Points', 'DraftKings Total Points Line', 'FanDuel Total Points Line'])
+    df = pd.DataFrame(matchups,columns=['Away Team','Home Team',f'Last {last_n_games} Games Avg Total Points', 'DraftKings Total Points Line', 'FanDuel Total Points Line'])
 
     print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
     
