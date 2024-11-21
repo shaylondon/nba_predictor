15fef0fc10709b3c9679ee239799c4bc2906cf5d
shay-london@uiowa.edu <112662764+shaylondon@users.noreply.github.com>
2024-11-20 22:25:17 -0600

    added 4 factor win prediction functionality

--- predictors/4factors_moneyline.py
+++ predictors/4factors_moneyline.py
@@ -10,15 +10,18 @@ from tabulate import tabulate
 from config import *
 from over_under import *
 
-def get_total_lines() -> list:
+def get_moneylines() -> list:
     json_data = get_betting_info()
     lines = []
     for i in range(len(json_data['events'])):
         team1 = json_data['events'][i]['teams'][0]['name'].split(' ')[-1]
         team2 = json_data['events'][i]['teams'][1]['name'].split(' ')[-1]
-        dk_total = json_data['events'][i]['lines']['19']['total']['total_over']
-        fd_total = json_data['events'][i]['lines']['23']['total']['total_over']
-        lines.append([team1, team2, dk_total, fd_total])
+        dk_spread = json_data['events'][i]['lines']['19']['spread']['point_spread_away']
+        dk_ml1 = json_data['events'][i]['lines']['19']['moneyline']['moneyline_away']
+        dk_ml2 = json_data['events'][i]['lines']['19']['moneyline']['moneyline_home']
+        fd_ml1 = json_data['events'][i]['lines']['23']['moneyline']['moneyline_away']
+        fd_ml2 = json_data['events'][i]['lines']['23']['moneyline']['moneyline_home']
+        lines.append([team1, team2, dk_spread, dk_ml1, dk_ml2, fd_ml1, fd_ml2])
     return lines
 
 def get_team_4factors_stats_last_n_games(games: int) -> DataFrame:
@@ -30,37 +33,48 @@ def get_team_4factors_stats_last_n_games(games: int) -> DataFrame:
 
     return team_4factors_stats.get_data_frames()[0]
 
-# def overs_last_n_games(top_n_teams: int, last_n_games=None) -> list:
-#     top_10_efg_last_n_games = efg_last_n_games(last_n_games)[:top_n_teams]
-#     top_10_pace_last_n_games = pace_last_n_games(last_n_games)[:top_n_teams]
-#     matchups = get_matchups()
-#     teams_playing = get_teams_playing()
-#
-#     last_10_efg_matchups = [entry.split(' ')[-1] for entry in top_10_efg_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
-#     last_10_pace_matchups = [entry.split(' ')[-1] for entry in top_10_pace_last_n_games['TEAM_NAME'] if entry.split(' ')[-1] in teams_playing]
-#
-#     return [matchup for matchup in matchups if (matchup[0] in last_10_efg_matchups and matchup[1] in last_10_pace_matchups) or (matchup[0] in last_10_pace_matchups and matchup[1] in last_10_efg_matchups)]
+def calculate_4factor_score(team: str, games=None) -> float:
+    factors_df = get_team_4factors_stats_last_n_games(games)
+    factors_df['TEAM_NAME'] = factors_df.apply(lambda row: row.TEAM_NAME.split(' ')[-1], axis=1)
+    factors_df.set_index('TEAM_NAME',inplace=True)
 
-# def efg_last_n_games(games: int) -> DataFrame:
-#     team_advanced_stats = get_team_advanced_stats_last_n_games(games)
-#     efg = team_advanced_stats[['TEAM_NAME','EFG_PCT_RANK', 'EFG_PCT']].sort_values(by='EFG_PCT_RANK')
-#     print(efg[:10])
-#
-#     return efg
+    efg_pct: float = factors_df['EFG_PCT'].get(team)
+    to_pct: float = factors_df['TM_TOV_PCT'].get(team)
+    off_reb_pct: float = factors_df['OREB_PCT'].get(team)
+    fta_rate: float = factors_df['FTA_RATE'].get(team)
+    pro_score: float = 0.44599*efg_pct - 0.341605*to_pct + 0.155112*off_reb_pct + 0.057293*fta_rate
+
+    op_efg_pct: float = factors_df['OPP_EFG_PCT'].get(team)
+    op_to_pct: float = factors_df['OPP_TOV_PCT'].get(team)
+    def_reb_pct: float = 1 - factors_df['OPP_OREB_PCT'].get(team)
+    op_fta_rate: float = factors_df['OPP_FTA_RATE'].get(team)
+    anti_score: float = -0.44599*op_efg_pct + 0.341605*op_to_pct + 0.155112*def_reb_pct - 0.057293*op_fta_rate
+
+    score: float = pro_score + anti_score
+    return score
+
 
 def main():
     last_n_games: int = int(input('Last _ Games: '))
-    matchups = overs_last_n_games(last_n_games)
-    if not matchups:
-        print('No matchups fit criteria on this date.')
-        return
-    for over_game in matchups:
-        for game in get_total_lines():
-            if over_game[0] == game[0]:
-                over_game.append(game[2])
-                over_game.append(game[3])
+    matchups = get_moneylines()
+    for matchup in matchups:
+        matchup.insert(2, calculate_4factor_score(matchup[1], last_n_games))
+        matchup.insert(2, calculate_4factor_score(matchup[0], last_n_games))
 
-    df = pd.DataFrame(matchups,columns=['Away Team','Home Team',f'Last {last_n_games} Games Avg Total Points', 'DraftKings Total Points Line', 'FanDuel Total Points Line'])
+    # for potential_game in matchups:
+    #     for game in get_moneylines():
+    #         if potential_game[0] == game[0]:
+    #             potential_game.append(game[2])
+    #             potential_game.append(game[3])
+    #             potential_game.append(game[4])
+    #             potential_game.append(game[5])
+    #             potential_game.append(game[6])
+
+    df = pd.DataFrame(matchups,columns=['Away Team','Home Team',
+                                        f'Away Last {last_n_games} Games 4 Factor Rating',
+                                        f'Home Last {last_n_games} Games 4 Factor Rating',
+                                        'DK Away Spread',
+                                        'DK Away ML', 'DK Home ML','FD Away ML', 'FD Home ML'])
 
     print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
     
